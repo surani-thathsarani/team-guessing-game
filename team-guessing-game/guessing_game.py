2c37ac9f7a67a49998ca7d715b36754f3c39a474
DinethyaHemarathne <nethmidinethya7@gmail.com>
2025-05-03 08:38:58 +0530

    Update guessing_game.py
    Line 6-9

--- team-guessing-game/guessing_game.py
+++ team-guessing-game/guessing_game.py
@@ -3,6 +3,10 @@ while True:
 number = random.randint(1, 100)
 print("Guess a number between 1 and 100")
 guess = int(input())
+if guess < number: 
+    print("Too low!") 
+elif guess > number: 
+    print("Too high!")
 if guess == number:
  print("You win!")
 else:
